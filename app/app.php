8d099b69f68d3f8527a278e91f6f67a16e1d118c
jdc <jdcxxx@gmail.com>
2017-03-03 10:37:54 -0800

    create silex routes and twig pages for brand crud functionality

--- app/app.php
+++ app/app.php
@@ -3,6 +3,7 @@ date_default_timezone_set('America/Los_Angeles');
 
 require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/../src/Store.php";
+require_once __DIR__ . "/../src/Brand.php";
 
 $app = new Silex\Application();
 
@@ -26,24 +27,47 @@ $app->get("/view_stores", function() use ($app) {
     $stores = Store::getAll();
     return $app['twig']->render('stores.html.twig', array('stores' => $stores));
 });
+$app->get("/view_brands", function() use ($app) {
+    $brands = Brand::getAll();
+    return $app['twig']->render('brands.html.twig', array('brands' => $brands));
+});
 $app->get("/edit_store/{id}", function($id) use ($app) {
     $store = Store::find($id);
     return $app['twig']->render('edit_store.html.twig', array('store' => $store));
+});
+$app->get("/edit_brand/{id}", function($id) use ($app) {
+    $brand = Brand::find($id);
+    return $app['twig']->render('edit_brand.html.twig', array('brand' => $brand));
 });
 $app->patch("/edit_store_name/{id}", function($id) use ($app) {
     $store = Store::find($id);
     $store->updateName($_POST['new_name']);
     return $app->redirect('/edit_store/' . $id);
 });
+$app->patch("/edit_brand_name/{id}", function($id) use ($app) {
+    $brand = Brand::find($id);
+    $brand->updateName($_POST['new_name']);
+    return $app->redirect('/edit_brand/' . $id);
+});
 $app->delete("/delete_store/{id}", function($id) use ($app) {
     $store = Store::find($id);
     $store->delete();
     return $app->redirect('/view_stores');
 });
+$app->delete("/delete_brand/{id}", function($id) use ($app) {
+    $brand = Brand::find($id);
+    $brand->delete();
+    return $app->redirect('/view_brands');
+});
 $app->post("/add_store", function() use ($app) {
     $store = new Store($_POST['name']);
     $store->save();
     return $app->redirect('/view_stores');
+});
+$app->post("/add_brand", function() use ($app) {
+    $brand = new Brand($_POST['name']);
+    $brand->save();
+    return $app->redirect('/view_brands');
 });
 return $app;
 ?>
