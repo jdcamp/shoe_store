d0c40147adaafc768a33ec8c2b9e60f954f70d74
jdc <jdcxxx@gmail.com>
2017-03-03 09:17:03 -0800

    implement and test Store.php save, getAll and deleteAll functions

--- src/Store.php
+++ src/Store.php
@@ -24,6 +24,28 @@
         {
     		return $this->id;
     	}
+        function save()
+    	{
+    		$GLOBALS['DB']->exec("INSERT INTO stores (name) VALUES ('{$this->getName()}')");
+    		$this->id = $GLOBALS['DB']->lastInsertId();
+    	}
+        static function getAll()
+    	{
+    		$returned_store = $GLOBALS['DB']->query("SELECT * FROM stores;");
+    		$stores = array();
+    		foreach($returned_store as $store)
+    		{
+    			$id = (int)$store['id'];
+    			$store_name = $store['name'];
+    			$new_store = new Store($store_name, $id);
+    			array_push($stores, $new_store);
+    		}
+    		return $stores;
+    	}
+        static function deleteAll()
+    	{
+    		$GLOBALS['DB']->exec("DELETE FROM stores;");
+    	}
 
 
     }
--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -7,13 +7,17 @@
 
 require_once "src/Store.php";
 
-// $server = 'mysql:host=localhost:8889;dbname=shoe_store_test';
-// $username = 'root';
-// $password = 'root';
-// $DB = new PDO($server, $username, $password);
+$server = 'mysql:host=localhost:8889;dbname=shoe_store_test';
+$username = 'root';
+$password = 'root';
+$DB = new PDO($server, $username, $password);
 
 class StoreTest extends PHPUnit_Framework_TestCase
 {
+    protected function teardown()
+    {
+        Store::deleteAll();
+    }
     function test_getName()
     {
         $name = 'Foo Store';
@@ -43,6 +47,42 @@ class StoreTest extends PHPUnit_Framework_TestCase
         $result = $test_store->getId();
 
         $this->assertEquals($id, $result);
+    }
+    function test_save()
+    {
+        $name = 'Foo Store';
+        $test_store = new Store($name);
+
+        $test_store->save();
+        $result = Store::getAll();
+
+        $this->assertEquals([$test_store], $result);
+    }
+    function test_getAll()
+    {
+        $name = 'Foo Store';
+        $test_store = new Store($name);
+        $name2 = 'Foo Store';
+        $test_store2 = new Store($name2);
+
+        $test_store->save();
+        $test_store2->save();
+
+        $result = Store::getAll();
+
+        $this->assertEquals([$test_store, $test_store2], $result);
+    }
+    function test_deleteAll()
+    {
+        $name = 'Foo Store';
+        $test_store = new Store($name);
+
+        $test_store->save();
+        Store::deleteAll();
+
+        $result = Store::getAll();
+
+        $this->assertEquals([], $result);
     }
 }
 
