6f859278749e3edc6e97d3b50c48f315493571b5
jdc <jdcxxx@gmail.com>
2017-03-03 09:54:43 -0800

    create silex routes and twig pages for adding stores

---
+++ app/app.php
@@ -0,0 +1,35 @@
+<?php
+date_default_timezone_set('America/Los_Angeles');
+
+require_once __DIR__ . "/../vendor/autoload.php";
+require_once __DIR__ . "/../src/Store.php";
+
+$app = new Silex\Application();
+
+$app['debug'] = true;
+
+$server = 'mysql:host=localhost:8889;dbname=shoe_store';
+$username = 'root';
+$password = 'root';
+$DB = new PDO($server, $username, $password);
+
+use Symfony\Component\HttpFoundation\Request;
+Request::enableHttpMethodParameterOverride();
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+));
+$app->get("/", function() use ($app) {
+    return $app['twig']->render('index.html.twig');
+});
+$app->get("/view_stores", function() use ($app) {
+    $stores = Store::getAll();
+    return $app['twig']->render('stores.html.twig', array('stores' => $stores));
+});
+$app->post("/add_store", function() use ($app) {
+    $store = new Store($_POST['name']);
+    $store->save();
+    return $app->redirect('/view_stores');
+});
+return $app;
+?>
